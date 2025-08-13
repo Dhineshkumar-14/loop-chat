111c109112758750bd14ac5a5b699da0edb69179
Dhineshkumar <dhineshkumar.velayudham@gmail.com>
2025-08-13 16:52:03 +0530

    Completed the signup api

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,50 @@
-export const signup = (req, res) => {
-  res.send("Sigup route");
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../lib/utils.js";
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All Fileds are required" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({ message: "Email is already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+      return res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+      });
+    } else {
+      return res.status(400).json({ message: "Invalid User Data" });
+    }
+  } catch (error) {
+    console.log("Error in singup controller" + error.message);
+    return res.status(500).json({ message: "Internal Server Exception" });
+  }
 };
 
 export const login = (req, res) => {
