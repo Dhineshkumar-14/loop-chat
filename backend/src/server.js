2ab744b6bb82cd8d906067cd6beffe7f88c2da44
Dhineshkumar <dhineshkumar.velayudham@gmail.com>
2025-08-19 16:40:20 +0530

    Fixed issues in deployment process

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,7 @@ import connectDB from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.io.js";
+import path from "path";
 
 dotenv.config();
 
@@ -23,6 +24,15 @@ app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+  });
+}
+
 server.listen(5001, () => {
   console.log("server is running on port " + PORT);
   connectDB();
--- frontend/src/config/url.config.js
+++ frontend/src/config/url.config.js
@@ -1,1 +1,3 @@
-export const BASE_URL='http://localhost:5001'
+export const BASE_URL = (import.meta.env.MODE = "development"
+  ? "http://localhost:5001/api"
+  : "/api");
