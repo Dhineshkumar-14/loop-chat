a4fa8dca5ec26dd2ba4da261695a3db095874720
Dhineshkumar <dhineshkumar.velayudham@gmail.com>
2025-08-21 15:29:12 +0530

    Changed the login page css

--- frontend/src/pages/view/LoginPage.jsx
+++ frontend/src/pages/view/LoginPage.jsx
@@ -30,43 +30,39 @@ const LoginPage = () => {
 
   return (
     <div className="login">
-      <div className="login-container">
-        <div className="login-formbox">
-          <form>
-            <h1>Login</h1>
-            <div className="input-box">
-              <input
-                type="text"
-                placeholder="Email"
-                required
-                name="email"
-                onBlur={onBlur}
-              />
-              <i className="bx bxs-envelope"></i>
-            </div>
-            <div className="input-box">
-              <input
-                type="text"
-                placeholder="password"
-                required
-                name="password"
-                onBlur={onBlur}
-              />
-              <i className="bx bxs-lock"></i>
-            </div>
-            <button className="btn" onClick={onLogin}>
-              Login{" "}
-            </button>
-            <p>
-              {" "}
-              don't have an account?{" "}
-              <strong>
-                <Link to={"/signup"}>signup</Link>
-              </strong>
-            </p>
-          </form>
+      <form>
+        <h1>Login</h1>
+        <div className="input-box">
+          <input
+            type="text"
+            placeholder="Email"
+            required
+            name="email"
+            onBlur={onBlur}
+          />
+          <i className="bx bxs-envelope"></i>
         </div>
-      </div>
+        <div className="input-box">
+          <input
+            type="text"
+            placeholder="password"
+            required
+            name="password"
+            onBlur={onBlur}
+          />
+          <i className="bx bxs-lock"></i>
+        </div>
+        <button className="btn" onClick={onLogin}>
+          Login{" "}
+        </button>
+        <p>
+          {" "}
+          don't have an account?{" "}
+          <strong>
+            <Link to={"/signup"}>signup</Link>
+          </strong>
+        </p>
+      </form>
     </div>
   );
 };
